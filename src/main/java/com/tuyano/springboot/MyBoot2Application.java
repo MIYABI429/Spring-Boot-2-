4f07bb389017cfe9f06a1cf6a3702a3a7b0f3b53
miyabi <miyabi04299@gmail.com>
2020-05-31 16:33:28 +0900

    farst comitto

---
+++ src/main/java/com/tuyano/springboot/MyBoot2Application.java
@@ -0,0 +1,13 @@
+package com.tuyano.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoot2Application.class, args);
+	}
+
+}
