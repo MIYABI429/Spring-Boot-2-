8a75bdc092fcbd2a163de7b185b21c165480ecd8
miyabi <miyabi04299@gmail.com>
2020-05-31 20:09:47 +0900

    コントローラー修正

---
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -0,0 +1,10 @@
+package com.tuyano.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.annotationRequestMapping;
+public class HeloController {
+	
+	@Controller
+	public cloass HeloCont
+
+}
