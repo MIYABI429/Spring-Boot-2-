467b0befc2c7cf539c484b65ecbd93f79347e309
miyabi <miyabi04299@gmail.com>
2020-06-02 18:05:44 +0900

    DataObjectを出力する

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,24 +1,49 @@
 package com.tuyano.springboot;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
+@RestController
 public class HeloController {
+	String[] names = {"tuyano",
+			"hanako","taro",
+			"sachikok","iciro"};
+	String[] mails = {"syoda@tuuyano.com",
+			"hanako@flower","taro@yamda",
+			"sachiko@happy","ichiro@baseball"};
+	
+	@RequestMapping("/{id}")
+	public DataObject index(@PathVariable int id) {
+		return new DataObject(id,names[id],mails[id]);
+	}
+}
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
-		mav.setViewName("index");
-		return mav;
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
+	
+	public DataObject(int id, String name, String value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
 	}
-	@RequestMapping("/other")
-	public String other() {
-		return "redirect:/";
-	}
-	@RequestMapping("/home")
-	public String home() {
-		return "forward:/";
-	}
+ public int getId() { return id;}
+ 
+ public void setId(int id) { this.id = id; }
+ 
+ public String getName() { return name; }
+ 
+ public void setName(String name) {
+	 this.name = name;
+ }
+ public String getValue() {
+	 return value;
+ }
+ 
+ public void setValue(String value) {
+	 
+ }
 }
