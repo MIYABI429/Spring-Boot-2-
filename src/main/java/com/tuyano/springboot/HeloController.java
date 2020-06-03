fc306ced9d22fa3cd97925343a49c32183a765e1
miyabi <miyabi04299@gmail.com>
2020-06-03 17:52:00 +0900

    テンプレート修正

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,8 +1,8 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HeloController {
@@ -12,10 +12,14 @@ public class HeloController {
 	String[] mails = {"syoda@tuuyano.com",
 			"hanako@flower","taro@yamda",
 			"sachiko@happy","ichiro@baseball"};
-	
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-		return new DataObject(id,names[id],mails[id]);
+
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg","current data.");
+		DataObject obj = new DataObject(123, "hanako","hanako@flower");
+		mav.addObject("object",obj);
+		return mav;
 	}
 }
 
@@ -23,7 +27,7 @@ class DataObject {
 	private int id;
 	private String name;
 	private String value;
-	
+
 	public DataObject(int id, String name, String value) {
 		super();
 		this.id = id;
@@ -31,19 +35,19 @@ class DataObject {
 		this.value = value;
 	}
  public int getId() { return id;}
- 
+
  public void setId(int id) { this.id = id; }
- 
+
  public String getName() { return name; }
- 
+
  public void setName(String name) {
 	 this.name = name;
  }
  public String getValue() {
 	 return value;
  }
- 
+
  public void setValue(String value) {
-	 
+
  }
 }
