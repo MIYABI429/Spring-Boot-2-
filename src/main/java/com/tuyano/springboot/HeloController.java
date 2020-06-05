39e90b42819fe4dbdce0fe31c8bf9e2afb80061a
miyabi <miyabi04299@gmail.com>
2020-06-05 14:10:38 +0900

    th:if/th:nulessを使う

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -18,9 +18,9 @@ public class HeloController {
 	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("id",id);
-		mav.addObject("check",id % 2 == 0);
-		mav.addObject("trueVal","Even numver!");
-		mav.addObject("falseVal","Odd numver...");
+		mav.addObject("check",id >= 0);
+		mav.addObject("trueVal","POSITIVE");
+		mav.addObject("falseVal","negative...");
          return mav;
 
 	}
