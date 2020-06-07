835385cdc68571915570d7db86d31fcb5c56e0a4
miyabi <miyabi04299@gmail.com>
2020-06-08 07:49:46 +0900

    繰り返しの「th:each}

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.ArrayList;
 
 @RestController
 public class HeloController {
@@ -14,14 +15,15 @@ public class HeloController {
 			"hanako@flower","taro@yamda",
 			"sachiko@happy","ichiro@baseball"};
 
-	@RequestMapping("/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		int m = Math.abs(month) % 12;
-		m = m == 0 ? 12 : m;
-		mav.addObject("month",m);
-		mav.addObject("check",Math.floor(m / 3));
-         return mav;
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"taro","taro@ymada","090-999-999"});
+		data.add(new String[] {"hanako","hanako@flower","080-888-888"});
+		data.add(new String[] {"sachiko","sachiko@happy","080-888-888"});
+		mav.addObject("data",data);
+		}
 
 	}
 
