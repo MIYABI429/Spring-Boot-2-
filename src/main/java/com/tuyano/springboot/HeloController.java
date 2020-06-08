80884438b0e5a9f73493911cd1b3e62e9c9c68ae
miyabi <miyabi04299@gmail.com>
2020-06-08 12:36:35 +0900

    フラグメントを組み込む（修正）

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,7 +1,5 @@
 package com.tuyano.springboot;
 
-import java.util.ArrayList;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,11 +16,6 @@ public class HeloController {
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		ArrayList<DataObject> data = new ArrayList<DataObject>();
-		data.add(new DataObject(0,"taro","taro@yamada"));
-		data.add(new DataObject(1,"hanako","hanako@flower"));
-		data.add(new DataObject(2,"sachiko","sachiko@happpy"));
-		mav.addObject("data",data);
 		return mav;
 
 	}
