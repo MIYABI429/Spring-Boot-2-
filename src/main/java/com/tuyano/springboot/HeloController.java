a65b4ae6b3bc79fa3ab12caee8d395439c6af15e
miyabi <miyabi04299@gmail.com>
2020-06-13 15:25:58 +0900

    リスト5-13

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -2,10 +2,15 @@ package com.tuyano.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tuyano.springboot.repositories.MyDataRepository;
+
+
 
 @Controller
  public class HeloController{
@@ -17,8 +22,10 @@ import com.tuyano.springboot.repositories.MyDataRepository;
 
 
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(
+		@ModelAttribute("foromModel") MyData mydata,
+		ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg","this is sample content.");
 		Iterable<MyData> list = repository.findAll();
@@ -26,6 +33,15 @@ import com.tuyano.springboot.repositories.MyDataRepository;
 		return mav;
 
 	}
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView form(
+			@ModelAttribute("formModel") MyData mydata,
+			ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+	    return new ModelAndView("redirect:/");
+	}
+
 }
 
 
