cf18dc8a9e202633198db0eececba1a8a029cacd
miyabi <miyabi04299@gmail.com>
2020-06-13 12:25:51 +0900

    リポジトリを利用する

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,52 +1,31 @@
 package com.tuyano.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-public class HeloController {
-	String[] names = {"tuyano",
-			"hanako","taro",
-			"sachikok","iciro"};
-	String[] mails = {"syoda@tuuyano.com",
-			"hanako@flower","taro@yamda",
-			"sachiko@happy","ichiro@baseball"};
+import com.tuyano.springboot.repositories.MyDataRepository;
+
+@Controller
+ public class HeloController{
+
+
+   @Autowired
+   MyDataRepository repository;
+
+
+
 
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg","this is sample content.");
+		Iterable<MyData> list = repository.findAll();
+		mav.addObject("data",list);
 		return mav;
 
 	}
-
-
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
- public int getId() { return id;}
-
- public void setId(int id) { this.id = id; }
-
- public String getName() { return name; }
-
- public void setName(String name) {
-	 this.name = name;
- }
- public String getValue() {
-	 return value;
- }
-
- public void setValue(String value) {
-
- }
 }
-}
+
+
