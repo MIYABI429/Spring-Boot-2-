19f052a47d74393c7eb7127ba6ff9d2ebeb3ba71
miyabi <miyabi04299@gmail.com>
2020-06-14 09:42:46 +0900

    リクエストハンドラの作成

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,11 +1,14 @@
 package com.tuyano.springboot;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -54,6 +57,24 @@ import com.tuyano.springboot.repositories.MyDataRepository;
 
 
 	}
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute MyData mydata,
+			@PathVariable int id,ModelAndView mav) {
+		mav.setViewName("edit");
+		mav.addObject("title","edit mydata.");
+		Optional<MyData> data = repository.findById((long)id);
+		mav.addObject("formModel",data.get());
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView update(@ModelAttribute MyData mydata,
+			ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+
 
 }
 
